bf197c2d11ac71ddb4fa145f45ebfb7612bbc075
Colin Powell <colingrahampowell@gmail.com>
2018-02-15 01:11:23 -0500

    finished process-enumerating capability

--- proc-analyzer.c
+++ proc-analyzer.c
@@ -4,6 +4,7 @@
  *  Process Analyzer
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
@@ -16,13 +17,13 @@
 #define FALSE 0
 
 typedef struct {
-
     char name[1024];
+    char cmdline[1024];
     int pid;
-
 } pinfo;
 
-int lefttrim(char *st);
+char *get_field_val(char *line);
+char* trim(char *st);
 int get_proc_info( char *path, pinfo *pi);
 int is_number(char *st);
 
@@ -40,23 +41,22 @@ int main(int argc, char **argv) {
     strcpy(name, "/proc");
     
     DIR *dptr = opendir(name);
+
+    printf("----PROCESSES----\n");
+    printf("%-8s%-32s%-64s\n", "PID", "NAME", "CMDLINE");
     
     while((entry = readdir(dptr)) != NULL) {
 
         // stat file
         stat(entry->d_name, &dir_stat);
 
-        // if filename is a number, read further
-//        strcpy(temp, entry->d_name);
-       
         if(is_number(entry->d_name) && S_ISDIR(dir_stat.st_mode)){
 
             snprintf(temp, sizeof(temp), "%s/%s", name, entry->d_name);
-            printf("found process id: %s\n", temp);
 
+            pi.pid = atoi(entry->d_name);
             get_proc_info(temp, &pi);
-
-
+            printf("%-8d%-32s%-64s\n", pi.pid, pi.name, pi.cmdline);
         } 
     } 
 
@@ -64,6 +64,97 @@ int main(int argc, char **argv) {
     dptr = NULL;
 
     return 0;
+}
+
+
+/*
+ * get_proc_info(): gets information about the process at the specified path, returns in a struct pinfo.
+ * NOTE: file reading logic adapted from getline() man pages, linux.die.net/man/3/getline
+ */
+
+int get_proc_info(char *path, pinfo *pi) {
+
+    char info_path[1024] = {'\0'};
+    strcpy(info_path, path);
+    strcat(info_path, "/cmdline");
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    // read command line info
+
+    FILE *fp = fopen(info_path, "r");
+    if(fp == NULL) {
+        fprintf(stderr, "ERROR: could not open file %s\n", info_path);
+        return FALSE;
+    }
+  
+    // read command 
+    while( (read = getline(&line, &len, fp)) != -1) {
+        strcpy(pi->cmdline, line);
+    }
+    fclose(fp);
+
+    strcpy(info_path, path);
+    strcat(info_path, "/status");
+
+    // open /proc/pid/status status for reading
+    fp = fopen(info_path, "r");
+
+    if(fp == NULL) {
+        fprintf(stderr, "ERROR: could not open file %s\n", info_path);
+        return FALSE;
+    } 
+
+    while( (read = getline(&line, &len, fp)) != -1) {
+
+        // if we have matched name field
+        if(strncmp("Name:", line, strlen("Name:")) == 0) {
+            strcpy(pi->name, get_field_val(line)); 
+            break;
+        }
+    }
+    
+    fclose(fp);
+
+    // free read line per getline() specs
+    free(line);
+    return TRUE;
+
+}
+
+/*
+ * lefttrim(): returns pointer to first non-whitespace char in string, 
+ * and trims any whitespace (incl. newlines) from end of string.
+ * NOTE: string must be null-terminated.
+ * some of this adapted from: https://stackoverflow.com/questions/
+ *      122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
+ */
+
+char *trim(char *st) {
+    
+    while(isspace((unsigned char)*st)) st++;
+    char *end = st + strlen(st) - 1;
+
+    // place null terminator at end 
+    while((end > st) && isspace((unsigned char)*end)) end--;
+    end = end + 1;
+    *end = '\0';
+
+    return st;
+}
+
+/*
+ * get_field_val(): gets value of parameter in field marked with "fld_name"
+ * - e.g., if field is "Name: foo", will return pointer to substring starting
+ * - with 'f'.
+ * NOTE: both parameters must be null-terminated strings.
+ */
+
+char *get_field_val(char *line) {
+    line = strpbrk(line, ":") + 1;
+    return trim(line);
 }
 
 /*
@@ -85,68 +176,3 @@ int is_number(char *st) {
 
 }
 
-
-/*
- * get_proc_info(): gets information about the process at the specified path, returns in a struct pinfo.
- * NOTE: file reading logic adapted from getline() man pages, linux.die.net/man/3/getline
- */
-
-int get_proc_info( char *path, pinfo *pi) {
-
-    char statuspath[1024] = {'\0'}; 
-  
-    strcpy(statuspath, path);
-    strcat(statuspath, "/status");
-
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    // open /proc/pid/status status for reading
-    FILE *fp = fopen(statuspath, "r");
-
-    if(fp == NULL) {
-        fprintf(stderr, "ERROR: could not open file %s\n", statuspath);
-        return FALSE;
-    } 
-
-    printf("getting information for process...\n");
-
-    while( (read = getline(&line, &len, fp)) != -1) {
-
-        // if we have matched name field
-        if(strncmp("Name:", line, strlen("Name:")) == 0) {
-
-            // trim off "Name:" along with any whitespace
-//            strcpy(pi->name, lefttrim( strcspn(line, ":") ));
-
-            // copy into "name" field of our process struct
-            printf("%s", line);
-        }
-        // otherwise, if we have matched Pid field
-        else if(strncmp("Pid:", line, strlen("pid:")) == 0){
-            printf("%s", line);
-        }
-    }
-    
-    fclose(fp);
-
-    return TRUE;
-
-}
-
-/*
- * lefttrim(): returns number of whitespace characters read  
- */
-
-int lefttrim(char *st) {
-
-    int i = 0;
-
-    while( strcspn(st, " \t") ) {
-        st++;
-        i++;
-    }
-    
-    return i;
-}
